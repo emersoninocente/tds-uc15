3849609d02c290fc049ca6009cc101bca4b660d2
Emerson Meneses Inocente <emerson_inocente@hotmail.com>
2025-10-23 17:09:56 -0300

    Update Aula 04

--- aula-04/code/js/script.js
+++ aula-04/code/js/script.js
@@ -1,20 +1,19 @@
 // Aguarda o documento HTML ser completamente carregado
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener('DOMContentLoaded', function () {
   const btnMobile = document.getElementById('btn-mobile');
   const nav = document.getElementById('nav');
 
   function toggleMenu(event) {
     // Evita o comportamento padrão de borbulhamento do evento (útil em formulários)
     if (event.type === 'touchstart') event.preventDefault();
-    
+
     // Adiciona ou remove a classe 'active' do elemento nav
     nav.classList.toggle('active');
 
     // Atualiza os atributos de acessibilidade
     const isActive = nav.classList.contains('active');
     btnMobile.setAttribute('aria-expanded', isActive);
-    
+
     if (isActive) {
       btnMobile.setAttribute('aria-label', 'Fechar Menu');
     } else {
@@ -26,5 +25,4 @@ document.addEventListener('DOMContentLoaded', function() {
   btnMobile.addEventListener('click', toggleMenu);
   // Adiciona o evento de toque (para melhor responsividade em dispositivos móveis)
   btnMobile.addEventListener('touchstart', toggleMenu);
-
 });
